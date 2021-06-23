ee380b0763cc3b7fffa1fb9e691e40e40cb9e9c2
Ben <Benmscott83@gmail.com>
2021-06-23 19:32:21 +0100

    update

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -1,41 +1,42 @@
 ﻿namespace ConsoleAppProject.App01
-{
+{ 
     /// <summary>
-    /// Please describe the main features of this App
+    /// This app will convert feet to miles
     /// </summary>
     /// <author>
-    /// Student Name version 0.1
+    /// Ben
     /// </author>
     public class DistanceConverter
-    {
+    { 
         private double miles;
 
         private double feet;
-
-    /// <summary>
-    ///
-    /// </summary>
-    
-        public void Run()
-    {
-
-    }
         /// <summary>
-        /// Promt the user to enter the distance in miles 
-        /// Input the miles as a double number
-        ///</summary>
-        private void InputMiles()
-    {
+        /// 
+        /// </summary>
+        public void Run()
+        {
 
-    }
+        }
+
+        /// <summary>
+        /// Prompt the user to enter the miles 
+        /// Input the miles as a double number
+        /// </summary>
+       
+        private void InputMiles()
+        {
+        
+        }
     
         private void CalculateFeet()
-    {
+        {
 
-    }
+        }
         private void OutputFeet()
-    {
+        {
 
 
+        }
     }
 }

23df0eba7769864a5e711858c0c7ef095ced0499
Ben <Benmscott83@gmail.com>
2021-06-23 18:42:00 +0100

    Latest update

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -8,6 +8,34 @@
     /// </author>
     public class DistanceConverter
     {
+        private double miles;
+
+        private double feet;
+
+    /// <summary>
+    ///
+    /// </summary>
+    
+        public void Run()
+    {
+
+    }
+        /// <summary>
+        /// Promt the user to enter the distance in miles 
+        /// Input the miles as a double number
+        ///</summary>
+        private void InputMiles()
+    {
+
+    }
+    
+        private void CalculateFeet()
+    {
+
+    }
+        private void OutputFeet()
+    {
+
 
     }
 }

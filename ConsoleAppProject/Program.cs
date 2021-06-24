76e06e443ec7b267760ced5ec1b7d3514a306ef4
Ben <Benmscott83@gmail.com>
2021-06-24 08:45:17 +0100

    test update

--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -1,5 +1,4 @@
-﻿using ConsoleAppProject.App03;
-using ConsoleAppProject.Helpers;
+﻿using ConsoleAppProject.App01;
 using System;
 
 namespace ConsoleAppProject
@@ -20,6 +19,9 @@ namespace ConsoleAppProject
             
             Console.WriteLine("BNU CO453 Applications Programming 2020-2021!");
             Console.WriteLine();
+            Console.Beep();
+
+            DistanceConverter converter = new DistanceConverter();
 
             // Using an extension method for each enumeration
             Console.WriteLine("Using MyEnum Extension Method!\n");
